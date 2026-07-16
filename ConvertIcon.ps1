14df6d63bc0c5a4db30613964a16ace024e25649
Vijay Machkuri <vijaymachkuri@github.com>
2026-07-16 07:16:27 +0530

    Fixed icon generation to perfectly preserve aspect ratio and maintain transparent background

--- ConvertIcon.ps1
+++ ConvertIcon.ps1
@@ -3,8 +3,31 @@ $pngPath = 'c:\Users\vijay\Desktop\securepro\Vcrypt.UI\icon.png'
 $icoPath = 'c:\Users\vijay\Desktop\securepro\Vcrypt.UI\icon.ico'
 
 $img = [System.Drawing.Image]::FromFile($pngPath)
-$resized = [System.Drawing.Bitmap]::new($img, 256, 256)
 
+# Calculate scale to preserve aspect ratio within 256x256
+$targetSize = 256
+$ratioX = $targetSize / $img.Width
+$ratioY = $targetSize / $img.Height
+$ratio = [Math]::Min($ratioX, $ratioY)
+
+$newWidth = [int]($img.Width * $ratio)
+$newHeight = [int]($img.Height * $ratio)
+
+# Create a new blank 256x256 transparent bitmap
+$bmp = [System.Drawing.Bitmap]::new($targetSize, $targetSize)
+$bmp.MakeTransparent()
+
+# Draw the scaled image into the center
+$graphics = [System.Drawing.Graphics]::FromImage($bmp)
+$graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+$graphics.Clear([System.Drawing.Color]::Transparent)
+
+$posX = ($targetSize - $newWidth) / 2
+$posY = ($targetSize - $newHeight) / 2
+
+$graphics.DrawImage($img, $posX, $posY, $newWidth, $newHeight)
+
+# Create ICO file
 $stream = [System.IO.FileStream]::new($icoPath, [System.IO.FileMode]::Create)
 $stream.WriteByte(0); $stream.WriteByte(0)
 $stream.WriteByte(1); $stream.WriteByte(0)
@@ -16,7 +39,7 @@ $stream.WriteByte(1); $stream.WriteByte(0)
 $stream.WriteByte(32); $stream.WriteByte(0)
 
 $ms = [System.IO.MemoryStream]::new()
-$resized.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png)
+$bmp.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png)
 $bytes = $ms.ToArray()
 $size = $bytes.Length
 
@@ -25,8 +48,9 @@ $stream.Write([System.BitConverter]::GetBytes([int]22), 0, 4)
 $stream.Write($bytes, 0, $size)
 
 $stream.Close()
-$resized.Dispose()
+$graphics.Dispose()
+$bmp.Dispose()
 $img.Dispose()
 $ms.Dispose()
 
-Write-Output "Icon created successfully!"
+Write-Output "Icon created successfully with correct aspect ratio and transparency!"
